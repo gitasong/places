7bc87dd49fb664566186fe04bca73eda62fccadf
Nicole Freed and Rena Rallis <nicole@>
2017-06-28 08:19:51 -0700

    add basic project structure

---
+++ app/app.php
@@ -0,0 +1,20 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Place.php";
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+        return "Home";
+
+    });
+
+    
+
+    return $app;
+?>
